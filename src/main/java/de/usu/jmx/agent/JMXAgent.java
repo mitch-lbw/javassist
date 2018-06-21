6adee399aa5ef2643e1e03ab103a7992a8e2e60e
mitch-lbw <dna.fan@gmx.de>
2018-06-21 21:27:34 +0200

    Update JMXAgent.java
    edit javadoc

--- src/main/java/de/usu/jmx/agent/JMXAgent.java
+++ src/main/java/de/usu/jmx/agent/JMXAgent.java
@@ -8,41 +8,11 @@ import java.lang.management.ManagementFactory;
 
 import javax.management.MBeanServer;
 
-/**
- * <strong>Projekt: jmx.agent</strong><br />
- * <strong>Klasse: de.usu.jmx.agent.JMXAgent</strong><br />
- *
- * <hr noshade />
- * <strong>Beschreibung:</strong><br />
- * Beispiel-Klasse fuer die USU Code Conventions
- * <br />
- * <hr noshade />
- * <strong>Historie:</strong><br />
- * 27.01.2017;usimmeier;Erstellung<br />
- *
- * <br />
- * <hr noshade />
- * 
- * @since 1.4
- * @author usimmeier
- * @version 1.0
- */
 public class JMXAgent
 {
     public static void premain( String agentArgs ) throws Throwable
     {
         System.out.println( "----- premain -------" );
-//        Thread th = new Thread( new Runnable()
-//        {
-//
-//            public void run()
-//            {
-//                
-//            }
-//        } );
-//        th.start();
-//        
-//        return;
     }
     
     public static void premain( String agentArgs, Instrumentation ins ) throws Throwable
